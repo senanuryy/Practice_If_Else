431b42e02cd93b0dbda36b687dbfaa78882af0bf
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2024-12-19 17:29:36 +0300

    Add project files.

---
+++ Practice_If_Else/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PracticeIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Lütfen bir sayı giriniz: ");
+
+            string strSayi = Console.ReadLine();
+
+            int sayi = Convert.ToInt32(strSayi);
+
+            if (sayi > 10)
+            {  
+                Console.WriteLine("Girilen sayi 10'dan buyuktur.");                                  
+            }
+            else
+            { 
+                Console.WriteLine("Girilen sayi 10'dan kucuktur.");
+            }
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girilen sayi cifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayi tektir.");
+            } 
+        }
+
+    }
+}
